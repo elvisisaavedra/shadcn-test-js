91f56703bfa8c62913f82e907f4699850eb78464
Elvis Saavedra <elvisisaavedra@yape.com.pe>
2024-07-05 23:31:42 -0500

    add dialog

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { Button } from '@/components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const handleOnEscapeKeyDown = (event) => {
+    // console.log(event)
+    event.preventDefault()
+  }
 
   return (
     <>
@@ -12,6 +26,45 @@ function App() {
       </Button>
 
       <Button variant={'secondary'}>Secondary</Button>
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">Edit Profile</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]" onEscapeKeyDown={handleOnEscapeKeyDown} onPointerDownOutside={(event) => event.preventDefault()}>
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you are done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <label htmlFor="name" className="text-right">
+                Name
+              </label>
+              <input
+                id="name"
+                defaultValue="Pedro Duarte"
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <label htmlFor="username" className="text-right">
+                Username
+              </label>
+              <input
+                id="username"
+                defaultValue="@peduarte"
+                className="col-span-3"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </>
   )
 }

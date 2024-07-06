d220394a18ddff2a72af5cdbb5a91b3fede923e7
Elvis Saavedra <elvisisaavedra@yape.com.pe>
2024-07-05 23:10:04 -0500

    init shadcn

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+import { fileURLToPath } from 'url';
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })

ef6d753ca27a9727147ee81e02bd507c0f17589d
Elvis Saavedra <elvisisaavedra@yape.com.pe>
2024-07-06 00:00:21 -0500

    add sheet - v1

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,23 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from '@/components/ui/dialog'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
 import { Button } from '@/components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   const handleOnEscapeKeyDown = (event) => {
-    // console.log(event)
     event.preventDefault()
   }
 
@@ -31,7 +40,11 @@ function App() {
         <DialogTrigger asChild>
           <Button variant="outline">Edit Profile</Button>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]" onEscapeKeyDown={handleOnEscapeKeyDown} onPointerDownOutside={(event) => event.preventDefault()}>
+        <DialogContent
+          className="sm:max-w-[425px]"
+          onEscapeKeyDown={handleOnEscapeKeyDown}
+          onPointerDownOutside={(event) => event.preventDefault()}
+        >
           <DialogHeader>
             <DialogTitle>Edit profile</DialogTitle>
             <DialogDescription>
@@ -65,6 +78,39 @@ function App() {
           </DialogFooter>
         </DialogContent>
       </Dialog>
+
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline">Open</Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Edit profile</SheetTitle>
+            <SheetDescription>
+              Make changes to your profile here. Click save when you are done.
+            </SheetDescription>
+          </SheetHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <label htmlFor="name" className="text-right">
+                Name
+              </label>
+              <input id="name" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <label htmlFor="username" className="text-right">
+                Username
+              </label>
+              <input id="username" className="col-span-3" />
+            </div>
+          </div>
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button type="submit">Save changes</Button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
     </>
   )
 }
